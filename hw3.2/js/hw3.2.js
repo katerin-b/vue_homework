f4bb5fdf87eb1ddfb68bb76eb31b469ecb38f839
bosikova <katerinabosikova93@gmail.com>
2021-12-06 21:52:08 +0300

    Homework №3.2

---
+++ hw3.2/js/hw3.2.js
@@ -0,0 +1,67 @@
+"use strict";
+let services = [
+    {
+        title: "Женская стрижка на длинные волосы",
+        price: 2000
+    },
+    {
+        title: "Стрижка челки",
+        price: 300
+    },
+    {
+        title: "Детская стрижка (до 7 лет)",
+        price: 700
+    },
+    {
+        title: "Мужская стрижка",
+        price: 1000
+    },
+    {
+        title: "Коррекция бороды",
+        price: 1200
+    },
+    {
+        title: "Тонирование мужских волос",
+        price: 1500
+    },
+    {
+        title: "Окрашивание волос",
+        price: 5000
+    },
+    {
+        title: "Женский маникюр",
+        price: 1500
+    },
+    {
+        title: "Мужской маникюр",
+        price: 1700
+    },
+    {
+        title: "Педикюр",
+        price: 2500
+    },
+    {
+        title: "Покрытие гель-лаком",
+        price: 1500
+    }
+];
+let app = Vue.createApp({
+    data(){
+        return {
+            services: services,
+            userList: []
+        }
+
+    },
+    methods: { // userList
+        addToList(service){ // push
+            service.inList = true;
+            this.userList.push(service);
+        },
+        removeFromList(service){ // filter
+            service.inList = false;
+            this.userList = this.userList.filter(b => b.inList);
+        }
+    }
+});
+app.mount("#app"); // приложение Vue нужно монтировать в элемент id="app"
