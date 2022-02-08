e45789405837d98b71e2606e588fe8a44b03ee78
bosikova <katerinabosikova93@gmail.com>
2022-02-08 14:11:43 +0300

    Edits

--- hw3.2/js/hw3.2.js
+++ hw3.2/js/hw3.2.js
@@ -1,67 +1,71 @@
-"use strict";
+'use strict';
+
 let services = [
     {
-        title: "Женская стрижка на длинные волосы",
-        price: 2000
+        title: 'Женская стрижка на длинные волосы',
+        price: 2000,
     },
     {
-        title: "Стрижка челки",
-        price: 300
+        title: 'Стрижка челки',
+        price: 300,
     },
     {
-        title: "Детская стрижка (до 7 лет)",
-        price: 700
+        title: 'Детская стрижка (до 7 лет)',
+        price: 700,
     },
     {
-        title: "Мужская стрижка",
-        price: 1000
+        title: 'Мужская стрижка',
+        price: 1000,
     },
     {
-        title: "Коррекция бороды",
-        price: 1200
+        title: 'Коррекция бороды',
+        price: 1200,
     },
     {
-        title: "Тонирование мужских волос",
-        price: 1500
+        title: 'Тонирование мужских волос',
+        price: 1500,
     },
     {
-        title: "Окрашивание волос",
-        price: 5000
+        title: 'Окрашивание волос',
+        price: 5000,
     },
     {
-        title: "Женский маникюр",
-        price: 1500
+        title: 'Женский маникюр',
+        price: 1500,
     },
     {
-        title: "Мужской маникюр",
-        price: 1700
+        title: 'Мужской маникюр',
+        price: 1700,
     },
     {
-        title: "Педикюр",
-        price: 2500
+        title: 'Педикюр',
+        price: 2500,
     },
     {
-        title: "Покрытие гель-лаком",
-        price: 1500
-    }
+        title: 'Покрытие гель-лаком',
+        price: 1500,
+    },
 ];
 let app = Vue.createApp({
-    data(){
+    data() {
         return {
             services: services,
-            userList: []
-        }
-
+            totalPrice: 0,
+        };
     },
-    methods: { // userList
-        addToList(service){ // push
-            service.inList = true;
-            this.userList.push(service);
+    methods: {
+        getTotalPrice(checked, price) {
+            if (checked) this.totalPrice += price;
+            else this.totalPrice -= price;
         },
-        removeFromList(service){ // filter
-            service.inList = false;
-            this.userList = this.userList.filter(b => b.inList);
-        }
-    }
+    },
 });
-app.mount("#app"); // приложение Vue нужно монтировать в элемент id="app"
+
+app.mount('#app'); // приложение Vue нужно монтировать в элемент id="app"
+
+
+
+
+
+
+
